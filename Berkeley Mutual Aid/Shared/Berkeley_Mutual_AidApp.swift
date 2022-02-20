625b11e3ec1bfb99aa1a0a0e77ed460e67ca8bf5
Woojin Ko <woojinko@Woojins-MBP.attlocal.net>
2022-02-19 22:48:00 -0800

    first commit of starter iOS code

---
+++ Berkeley Mutual Aid/Shared/Berkeley_Mutual_AidApp.swift
@@ -0,0 +1,17 @@
+//
+//  Berkeley_Mutual_AidApp.swift
+//  Shared
+//
+//  Created by Woojin Ko on 2/19/22.
+//
+
+import SwiftUI
+
+@main
+struct Berkeley_Mutual_AidApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
